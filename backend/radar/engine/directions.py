a77e4a5486d418758d69208175c734edcb6f7050
Anton Korzh <antonkrzh@outlook.com>
2020-07-02 14:18:00 +0300

    Separate moving_objects' logic from directions', improve directions.py

---
+++ backend/radar/engine/directions.py
@@ -0,0 +1,98 @@
+import random
+from typing import Dict, Callable, Sequence, ClassVar
+from radar.engine.alien_bodies import Position
+from radar.engine import directions_meta
+
+
+class Direction:
+    def __init__(self,
+                 name: str,
+                 handler: Callable[[Position], Position],
+                 risk_zone_provider: Callable[[Position, int, int],
+                                              Sequence[Position]],
+                 opposite_key: str):
+        """
+        :param name: direction name
+        :param handler: function for moving from current position in this
+        direction
+        :param risk_zone_provider: points of the moving object
+        (assuming it's located in top left corner of an area)
+        that should be tested for collision
+        :param opposite_key: key for the opposite direction in DirectionsPool
+        """
+        self.name = name
+        self.move_from_pos = handler
+        self._risk_zone_provider = risk_zone_provider
+        self._opposite_key = opposite_key
+        self._opposite = None
+
+    @property
+    def opposite(self):
+        if self._opposite is None:
+            self._opposite = directions_pool[self._opposite_key]
+            del self._opposite_key
+            return self._opposite
+        else:
+            return self._opposite
+
+    def positions_to_occupy_by_movement(self, cur_pos, width, height):
+        return self._movement_affected_points(cur_pos, width, height,
+                                              self._risk_zone_provider)
+
+    def positions_freed_by_movement(self, new_pos, width, height):
+        return self._movement_affected_points(new_pos, width, height,
+                                              self.opposite._risk_zone_provider)
+
+    @staticmethod
+    def _movement_affected_points(pos, width, height, provider):
+        return provider(pos, width=width, height=height)
+
+
+class _DirectionPool:
+    __directions: ClassVar[Dict[str, Direction]] = {
+        'left':
+            Direction('Left',
+                      lambda cur_pos: Position(cur_pos.x - 1, cur_pos.y),
+                      directions_meta.left_risk_zone, 'right'),
+        'right':
+            Direction('Right',
+                      lambda cur_pos: Position(cur_pos.x + 1, cur_pos.y),
+                      directions_meta.right_risk_zone, 'left'),
+        'up':
+            Direction('UP',
+                      lambda cur_pos: Position(cur_pos.x, cur_pos.y - 1),
+                      directions_meta.upper_risk_zone, 'down'),
+        'down':
+            Direction('Down',
+                      lambda cur_pos: Position(cur_pos.x, cur_pos.y + 1),
+                      directions_meta.bottom_risk_zone, 'up'),
+        'top_left':
+            Direction('Top Left',
+                      lambda cur_pos: Position(cur_pos.x - 1, cur_pos.y - 1),
+                      directions_meta.top_left_risk_zone, 'bottom_right'),
+        'top_right':
+            Direction('Top Right',
+                      lambda cur_pos: Position(cur_pos.x + 1, cur_pos.y - 1),
+                      directions_meta.top_right_risk_zone, 'bottom_left'),
+        'bottom_left':
+            Direction('Bottom Left',
+                      lambda cur_pos: Position(cur_pos.x - 1, cur_pos.y + 1),
+                      directions_meta.bottom_left_risk_zone, 'top_right'),
+        'bottom_right':
+            Direction('Bottom Right',
+                      lambda cur_pos: Position(cur_pos.x + 1, cur_pos.y + 1),
+                      directions_meta.bottom_right_risk_zone, 'top_left')
+    }
+    values = tuple(__directions.values())
+
+    def random_direction(self) -> Direction:
+        return random.choice(self.values)
+
+    def __getattr__(self, item):
+        return self.__directions[item]
+
+    def __getitem__(self, item: str) -> Direction:
+        return self.__directions[item]
+
+
+directions_pool = _DirectionPool()
--- backend/radar/engine/moving_objects.py
+++ backend/radar/engine/moving_objects.py
@@ -1,61 +1,11 @@
-import random
-from typing import Set, Iterator
-from radar.engine.moving_objects_meta import Position, alien_bodies
-from radar.engine import directions_meta
-
-
-class _Direction:
-    def __init__(self, name, handler, risk_zone_provider, opposite_idx):
-        """
-        :param name: direction name
-        :param handler: function for moving from current position in this direction
-        :param risk_zone_provider: points of the moving object
-        (assuming it's located in top left corner of an area)
-        that should be tested for collision
-        :param opposite_idx: opposite direction index in _directions_list
-        """
-        self.name = name
-        self.move_from_pos = handler
-        self._risk_zone_provider = risk_zone_provider
-        self._opposite_idx = opposite_idx
-
-    def collision_risk_points(self, cur_pos, width, height):
-        return self._movement_affected_points(cur_pos, width, height,
-                                              self._risk_zone_provider)
-
-    def freed_after_movement_points(self, new_pos, width, height):
-        opposite_direction = _directions_list[self._opposite_idx]
-        return self._movement_affected_points(new_pos, width, height,
-                                              opposite_direction._risk_zone_provider)
-
-    @staticmethod
-    def _movement_affected_points(pos, width, height, provider):
-        return provider(pos, width=width, height=height)
-
-
-_directions_list = (
-    _Direction('Left', lambda cur_pos: Position(cur_pos.x - 1, cur_pos.y),
-               directions_meta.left_risk_zone, 1),
-    _Direction('Right', lambda cur_pos: Position(cur_pos.x + 1, cur_pos.y),
-               directions_meta.right_risk_zone, 0),
-    _Direction('UP', lambda cur_pos: Position(cur_pos.x, cur_pos.y - 1),
-               directions_meta.upper_risk_zone, 3),
-    _Direction('Down', lambda cur_pos: Position(cur_pos.x, cur_pos.y + 1),
-               directions_meta.bottom_risk_zone, 2),
-    _Direction('Top Left', lambda cur_pos: Position(cur_pos.x - 1, cur_pos.y - 1),
-               directions_meta.top_left_risk_zone, 7),
-    _Direction('Top Right', lambda cur_pos: Position(cur_pos.x + 1, cur_pos.y - 1),
-               directions_meta.top_right_risk_zone, 6),
-    _Direction('Bottom Left', lambda cur_pos: Position(
-        cur_pos.x - 1, cur_pos.y + 1), directions_meta.bottom_left_risk_zone, 5),
-    _Direction('Bottom Right', lambda cur_pos: Position(
-        cur_pos.x + 1, cur_pos.y + 1), directions_meta.bottom_right_risk_zone, 4)
-)
+from typing import Set, Optional
+from radar.engine.alien_bodies import Position, alien_bodies
+from radar.engine.directions import Direction, directions_pool
 
 
 class MovingObject:
-    def __init__(self, body_idx: int, position: Position = None):
-        self.direction: _Direction = random.choice(_directions_list)
+    def __init__(self, body_idx: int, position: Optional[Position] = None):
+        self.direction: Direction = directions_pool.random_direction()
         self.body_idx = body_idx
         self.body = alien_bodies[body_idx]
         self.height = self.body.height
@@ -63,13 +13,13 @@ class MovingObject:
         self.position: Position = position
 
     def estimate_movement(self) -> Set[Position]:
-        return self.direction.collision_risk_points(self.position,
-                                                    self.width,
-                                                    self.height)
+        return self.direction.positions_to_occupy_by_movement(self.position,
+                                                              self.width,
+                                                              self.height)
 
     def move(self) -> Set[Position]:
         self.position = self.direction.move_from_pos(self.position)
-        return self.direction.freed_after_movement_points(self.position,
+        return self.direction.positions_freed_by_movement(self.position,
                                                           self.width,
                                                           self.height)
 
@@ -79,7 +29,7 @@ class MovingObject:
     def _get_new_direction(self):
         old_direction = self.direction
         while self.direction == old_direction:
-            self.direction = random.choice(_directions_list)
+            self.direction = directions_pool.random_direction()
 
     def get_line_iterator(self):
         return iter(self.body.str_lines)
